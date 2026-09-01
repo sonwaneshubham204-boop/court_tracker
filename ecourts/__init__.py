194efa26e81007542ca4c4ad92ab1110ef9c9452
sonwaneshubham204-boop <sonwaneshubham204@gmail.com>
2026-09-01 22:05:58 +0530

    chore: add ecourts package placeholder (init)

---
+++ ecourts/__init__.py
@@ -0,0 +1,6 @@
+"""
+ecourts package - placeholder client, normalizer, service, and migrations
+This package provides the foundation for future eCourts integration.
+
+All network access is disabled here: the client is an interface/placeholder only.
+"""
